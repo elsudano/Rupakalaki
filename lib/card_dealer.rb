8a75540f9eecee9bf6e48ec1aeaff46b6fbaa747
usuario <usuario@sky.critihost.com>
2015-11-20 20:30:50 +0100

    subiendo todos los personajes del juego y poniendo los ficheros de texto en los sitios donde tienen que estar

--- lib/card_dealer.rb
+++ lib/card_dealer.rb
@@ -15,73 +15,79 @@ class CardDealer
   def initTreasureCardDeck()
     @unusedTreasures = Array.new
     @usedTreasures = Array.new
-     File.open('/resources/base_datos_tesoros.txt', 'r') do |f1|
-     while linea = f1.gets
-       linea.add()
-      end
-    
-    
-  end
-  
-    def initMonsterCardDeck()
-    @unusedMonsters = Array.new
-    @usedMonsters = Array.new
     File.open('/resources/base_datos_tesoros.txt', 'r') do |f1|
-     while linea = f1.gets
-       linea.add()
+      while linea = f1.gets
+        linea.add()
       end
-end
     end
-  def shuffleTreasures()
-    @unusedTreasures = @unusedTreasures.shuffle
-  end
+    
+    /*
+     * Esta es una funcion que sirve para inicializar el mazo de los 
+     * monstruos se esta intentando que se importen los monstruos
+     * directamente desde un fichero de texto para que no tener que
+     * crearlos todos a mano.
+     */
+    def initMonsterCardDeck()
+      @unusedMonsters = Array.new
+      @usedMonsters = Array.new
+      File.open('/resources/base_datos_tesoros.txt', 'r') do |f1|
+        while linea = f1.gets
+          linea.add()
+        end
+      end
+    end
+    
+    def shuffleTreasures()
+      @unusedTreasures = @unusedTreasures.shuffle
+    end
   
-  def shuffleMonsters()
-    @unusedMonsters = @unusedMonsters.shuffle
-  end
+    def shuffleMonsters()
+      @unusedMonsters = @unusedMonsters.shuffle
+    end
   
-  def nextTreasure()
-    if @unusedTreasures.empty?
+    def nextTreasure()
+      if @unusedTreasures.empty?
         @usedTreasures.each do |t| 
           @unusedTreasures<<t
         end
-    shuffleTreasures
-    @usedTreasures.clear
-    end
-    t = @unusedTreasures.at(0)
-    @usedTreasures<<t
-    @unusedTreasures.delete(t);
-    return t
-  end
-  
-  def nextMonster()
-    if @unusedMonsters.empty?
-      @usedMonsters.each do |m| 
-        @unusedMonsters<<m
+        shuffleTreasures
+        @usedTreasures.clear
       end
-      shuffleMonsters()
-      @usedTreasures.clear
+      t = @unusedTreasures.at(0)
+      @usedTreasures<<t
+      @unusedTreasures.delete(t);
+      return t
     end
-    m = @unusedMonsters.at(0)
-    @usedMonsters<<m
-    @unusedMonsters.delete(m);
-    return m
-  end
   
-  def giveTreasuresBack(t)
-    @usedTreasures<<t
-  end
+    def nextMonster()
+      if @unusedMonsters.empty?
+        @usedMonsters.each do |m| 
+          @unusedMonsters<<m
+        end
+        shuffleMonsters()
+        @usedTreasures.clear
+      end
+      m = @unusedMonsters.at(0)
+      @usedMonsters<<m
+      @unusedMonsters.delete(m);
+      return m
+    end
   
-  def giveMonstersBack(m)
-    @usedMonsters<<m
-  end
+    def giveTreasuresBack(t)
+      @usedTreasures<<t
+    end
   
-  def initCards()
-    initTreasureCardDeck()
-    shuffleTreasures()
-    initMonsterCardDeck()
-    shuffleMonsters()
-  end
+    def giveMonstersBack(m)
+      @usedMonsters<<m
+    end
   
-  private :initMonsterCardDeck, :initTreasureCardDeck, :shuffleMonsters, :shuffleTreasures
+    def initCards()
+      initTreasureCardDeck()
+      shuffleTreasures()
+      initMonsterCardDeck()
+      shuffleMonsters()
+    end
+  
+    private :initMonsterCardDeck, :initTreasureCardDeck, :shuffleMonsters, :shuffleTreasures
+  end
 end
